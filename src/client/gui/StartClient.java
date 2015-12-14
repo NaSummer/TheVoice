888675d9c9e8d493cb37d3bbbf325aed26e3c348
CharlesXuWenbo <zjhzxwb1996@gmail.com>
2015-12-14 16:23:37 +0800

    Interface(Inception,sign up,sign in,setting.....)
    disscusGround,traningmode........rest

--- src/client/gui/StartClient.java
+++ src/client/gui/StartClient.java
@@ -1,14 +1,28 @@
 package client.gui;
 
-import client.transmission.Client;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class StartClient {
 
+	JFrame jf = new JFrame();
+	Interface i = new Interface();
+	JPanel jp = i.InceptionInterface();
 	public static void main(String[] args) {
 		
-		new Inception();
-//		new DisscusGroundClass(new Client());
-		
+		JFrame jf = new JFrame();
+		Interface i = new Interface();
+		JPanel jp = i.InceptionInterface();
+		//new Inception();
+
+	
+		// 设置Frame
+				jf.setSize(600, 850); // 设置大小为 600 × 850
+				jf.add(jp);
+//				jf.setResizable(false); // 使窗口大小无法改变
+				jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 设置默认关闭操作
+				jf.setVisible(true);
+
 	}//end main
 
 }
