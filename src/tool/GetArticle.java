b7ae61737a392267ea28bd37f218c6819986a4ef
QiufanXu <519893120@qq.com>
2015-12-14 19:59:00 +0800

    Delete test files in server side

--- src/tool/GetArticle.java
+++ src/tool/GetArticle.java
@@ -1,11 +1,6 @@
 package tool;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
